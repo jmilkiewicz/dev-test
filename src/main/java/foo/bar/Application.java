aa5d53d9bee3d0c6437321fc2ef02ac17c684625
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2015-02-12 11:55:24 +0100

    make test pass

--- src/main/java/foo/bar/Application.java
+++ src/main/java/foo/bar/Application.java
@@ -1,21 +1,31 @@
 package foo.bar;
 
 
+import foo.bar.foo.bar.domain.Location;
 import foo.bar.service.LocationSource;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.LinkedList;
 
 public class Application {
 
     private final LocationSource locationSource;
 
     public Application(LocationSource locationSource) {
-
         this.locationSource = locationSource;
     }
 
-    public Collection<String> exportFor(String location) {
-        return Arrays.asList("1,location,51.45775,10.2384");
+    public Collection<String> exportFor(String locationName) {
+        Collection<String> result = new LinkedList<>();
+        Collection<Location> locations = locationSource.getLocationsFor("sda");
+        for (Location location : locations) {
+            String csv = location.getId() + "," + location.getType()+","+ location.getPosition().getLatitude() +"," +
+                    location.getPosition().getLongitude();
+            result.add(csv);
+        }
+
+
+        return result;
     }
 }
