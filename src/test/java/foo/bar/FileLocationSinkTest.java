2e49d667e703b073bbef4cd482036e67aab07e55
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2015-02-16 11:43:11 +0100

    test refactor

--- src/test/java/foo/bar/FileLocationSinkTest.java
+++ src/test/java/foo/bar/FileLocationSinkTest.java
@@ -3,6 +3,7 @@ package foo.bar;
 
 import foo.bar.external.FileLocationSink;
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.*;
@@ -13,29 +14,35 @@ import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class FileLocationSinkTest {
+    public static final String SAMPLE_FILE_NAME = "aaa.csv";
     private FileLocationSink sut = new FileLocationSink();
 
+    @Before
+    public void removeSampleFileName() throws Exception {
+        File file = new File(SAMPLE_FILE_NAME);
+        if(file.exists()){
+            file.delete();
+        }
+    }
+
     @Test
-    public void shallSaveDataToAFile() throws FileNotFoundException, UnsupportedEncodingException {
+    public void shallSaveDataToAFile() throws IOException {
         String[] fileContent = {"a1,a2", "b1,b2", "c1,c2"};
 
-        sut.storeLocations("aaa.csv", Arrays.asList(fileContent));
+        sut.storeLocations(SAMPLE_FILE_NAME, Arrays.asList(fileContent));
 
-        assertThat(readFileContent("aaa.csv"), Matchers.contains(fileContent));
+        assertThat(readFileContent(SAMPLE_FILE_NAME), Matchers.contains(fileContent));
     }
 
-    private List<String> readFileContent(String fileName) throws FileNotFoundException, UnsupportedEncodingException {
+    private List<String> readFileContent(String fileName) throws IOException {
         List<String> fileRows = new LinkedList<>();
-        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName),
-                "UTF-8"))){
+        try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName),
+                "UTF-8"))) {
             String line;
-            while( (line = bufferedReader.readLine()) != null){
+            while ((line = bufferedReader.readLine()) != null) {
                 fileRows.add(line);
             }
-        } catch (IOException e) {
-            e.printStackTrace();
+            return fileRows;
         }
-        return  fileRows;
     }
-
 }
