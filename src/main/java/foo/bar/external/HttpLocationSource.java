c9ec7a2f3d38c7afea6755db91cbd8dd902c3091
Jakub Milkiewicz <jmilkiewicz@gmail.com>
2015-02-16 10:57:41 +0100

    creating HTTPLocationSource so we have a component to read data from HTTP GoEuro API

---
+++ src/main/java/foo/bar/external/HttpLocationSource.java
@@ -0,0 +1,38 @@
+package foo.bar.external;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import foo.bar.foo.bar.domain.Location;
+import foo.bar.service.LocationSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Collection;
+
+
+public class HttpLocationSource implements LocationSource {
+    @Override
+    public Collection<Location> getLocationsFor(String location) {
+
+        try {
+            //TODO use string format instead of lame string concatenation
+            URL url = new URL("http://api.goeuro.com/api/v2/position/suggest/en/"+ location);
+            InputStream inputStream = url.openStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream,
+                    Charset.forName
+                            ("UTF-8"));
+            Gson gson = new Gson();
+            Type collectionType = new TypeToken<Collection<Location>>(){}.getType();
+            return gson.fromJson(inputStreamReader, collectionType);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("unable to parse URL", e);
+        } catch (IOException e) {
+            throw new RuntimeException("unable to readData", e);
+        }
+    }
+}
--- src/test/java/foo/bar/GoEuroAPITest.java
+++ src/test/java/foo/bar/GoEuroAPITest.java
@@ -1,13 +1,9 @@
 package foo.bar;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
+import foo.bar.external.HttpLocationSource;
+import foo.bar.foo.bar.domain.Location;
 import org.junit.Test;
 
-import java.io.*;
-import java.lang.reflect.Type;
-import java.net.URL;
-import java.nio.charset.Charset;
 import java.util.Collection;
 
 import static org.hamcrest.CoreMatchers.notNullValue;
@@ -16,36 +12,22 @@ import static org.hamcrest.Matchers.emptyCollectionOf;
 import static org.hamcrest.core.IsNot.not;
 
 public class GoEuroAPITest {
+    private HttpLocationSource sut = new HttpLocationSource();
 
     @Test
-    public void shallReturnNonEmptyJsonForUSA() throws Exception {
-        Collection<Location> usaLocations = getLocations("USA");
+    public void shallReturnValidCollectionOfLocationsForUSA() throws Exception {
+        Collection<Location> usaLocations = sut.getLocationsFor("USA");
 
         assertThat(usaLocations, notNullValue());
         assertThat(usaLocations, not(emptyCollectionOf(Location.class)));
-    }
 
-    private Collection<Location> getLocations(String location) throws IOException {
-        //TODO use string format instead of lame string concatenation
-        URL url = new URL("http://api.goeuro.com/api/v2/position/suggest/en/"+ location);
-        InputStream inputStream = url.openStream();
-        InputStreamReader inputStreamReader = new InputStreamReader(inputStream,
-                Charset.forName
-                        ("UTF-8"));
-
-        Gson gson = new Gson();
-
-        Type collectionType = new TypeToken<Collection<Location>>(){}.getType();
-
-        return gson.fromJson(inputStreamReader, collectionType);
     }
 
     @Test
     public void shallReturnEmptyJsonForNonExistingLocation() throws Exception {
-        Collection<Location> nonExistingLocations = getLocations("_SADADSA");
+        Collection<Location> nonExistingLocations = sut.getLocationsFor("_SADADSA");
 
         assertThat(nonExistingLocations, emptyCollectionOf(Location.class));
     }
-
 
 }
